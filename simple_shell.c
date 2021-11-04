6b7202185b0db59dcde369ba13149f2de66c69db
Afiya21 <NebyatAhmed21@gmail.com>
2021-11-04 14:19:14 +0300

    fixing batty

--- simple_shell.c
+++ simple_shell.c
@@ -1,80 +1,44 @@
 #include "shell.h"
+
 /**
-* copyarray - a function to copy string array to another string array variable
-* @line: a string array to be copied
-* 
-* Return: returns the copied array
-*/
-char **copyarray(char **line)
+* GetWord - get a word from a string to cpy
+* @str: pointer that point to a string
+* @w: pointer that point to pointer that point to a words
+* Return: array of words
+**/
+void GetWord(char **w, char *str)
 {
-char **array;
-int i = 1;
-array = malloc(64);
-if (!array)
-return (NULL);
-while (line[i] != NULL)
-{
-array[(i - 1)] = malloc(32);
-if (!array[(i - 1)])
-return (NULL);
-strcopy(line[i], array[(i - 1)]);
-i++;
-}
-return (array);
-}
-/**
-* sigintHandler - a function to handle the ctrl-c signal
-* @sig_num: an integer signal indicator
-*
-* Return: void function
-*/
-void sigintHandler(int sig_num __attribute__((unused)))
-{
-signal(SIGINT, sigintHandler);
-write(1, "\n", 2);
-printprompt(0);
-fflush(stdout);
-}
-/**
-* main - a the main function of the shell
-* @argc: the number of arguments given
-* @argv: an array of given argument strings
-*
-* Return: returns an integer
-*/
-int main(int argc __attribute__((unused)), char **argv)
-{
-char *line;
-line = malloc(256);
-if (!line)
-{
-perror("Allocation");
-exit(1);
-}
-if (!isatty(STDIN_FILENO))
-{
-if (getstr(line) == (-1))
-{
-write(1, "\n", 2);
-exit(1);
-}
-if (shellprocessor(strbrk(line, ' '), argv) == -1)
-{
-perror("Error");
-}
-exit(0);
-}
-do {
-printprompt(0);
-if (getstr(line) == (-1))
-{
-write(1, "\n", 2);
-exit(0);
-}
-if ((shellprocessor(strbrk(line, ' '), argv)) == -1)
-{
-perror("Error");
-}
-} while (1);
-return (0);
+	int i, start, find, count_w;
+
+	count_w = i = start = find = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i + 1] == '\0')
+		{
+			if (find)
+			{
+				if (str[i + 1] == '\0')
+					i += 1;
+				cpString(i - start, str + start, w + count_w);
+				if (!(w + count_w))
+					exit(-2);
+				count_w += 1;
+				start = i + 1;
+				find = 0;
+			}
+				if (str[i] == '\0')
+					continue;
+			i += 1;
+			continue;
+		}
+		else
+		{
+			if (!find)
+				start = i;
+			find = 1;
+		}
+		i += 1;
+	}
+	w[count_w] = NULL;
 }
